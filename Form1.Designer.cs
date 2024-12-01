56743287bdbc8cdfb9fe025a87121b27513170ad
nguye <nguye@DESKTOP-LP7Q8G7>
2024-12-01 15:19:02 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace invoice_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_InvoiceID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_SupplierID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_TotalAmount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvItems = new System.Windows.Forms.DataGridView();
+            this.ItemID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InvoiceID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UnitPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grImage = new System.Windows.Forms.DataGridViewImageColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tb_ProductID = new System.Windows.Forms.TextBox();
+            this.tb_Quantity = new System.Windows.Forms.TextBox();
+            this.tb_UnitPrice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_ItemID = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tb_add_pic = new System.Windows.Forms.Button();
+            this.tb_InvoiceDate = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItems)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_InvoiceID
+            // 
+            this.tb_InvoiceID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_InvoiceID.Location = new System.Drawing.Point(830, 47);
+            this.tb_InvoiceID.Name = "tb_InvoiceID";
+            this.tb_InvoiceID.Size = new System.Drawing.Size(154, 26);
+            this.tb_InvoiceID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(715, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "InvoiceID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(715, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "InvoiceDate";
+            // 
+            // tb_SupplierID
+            // 
+            this.tb_SupplierID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_SupplierID.Location = new System.Drawing.Point(830, 111);
+            this.tb_SupplierID.Name = "tb_SupplierID";
+            this.tb_SupplierID.Size = new System.Drawing.Size(154, 26);
+            this.tb_SupplierID.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(715, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "SupplierID";
+            // 
+            // tb_TotalAmount
+            // 
+            this.tb_TotalAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_TotalAmount.Location = new System.Drawing.Point(830, 143);
+            this.tb_TotalAmount.Name = "tb_TotalAmount";
+            this.tb_TotalAmount.Size = new System.Drawing.Size(154, 26);
+            this.tb_TotalAmount.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(715, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "TotalAmount";
+            // 
+            // dgvItems
+            // 
+            this.dgvItems.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvItems.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ItemID,
+            this.InvoiceID,
+            this.ProductID,
+            this.Quantity,
+            this.UnitPrice,
+            this.grImage});
+            this.dgvItems.Location = new System.Drawing.Point(66, 284);
+            this.dgvItems.Name = "dgvItems";
+            this.dgvItems.RowTemplate.Height = 70;
+            this.dgvItems.Size = new System.Drawing.Size(540, 317);
+            this.dgvItems.TabIndex = 2;
+            // 
+            // ItemID
+            // 
+            this.ItemID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ItemID.HeaderText = "ItemID";
+            this.ItemID.Name = "ItemID";
+            // 
+            // InvoiceID
+            // 
+            this.InvoiceID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.InvoiceID.HeaderText = "InvoiceID";
+            this.InvoiceID.Name = "InvoiceID";
+            // 
+            // ProductID
+            // 
+            this.ProductID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ProductID.HeaderText = "ProductID";
+            this.ProductID.Name = "ProductID";
+            // 
+            // Quantity
+            // 
+            this.Quantity.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Quantity.HeaderText = "Quantity";
+            this.Quantity.Name = "Quantity";
+            // 
+            // UnitPrice
+            // 
+            this.UnitPrice.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.UnitPrice.HeaderText = "UnitPrice";
+            this.UnitPrice.Name = "UnitPrice";
+            // 
+            // grImage
+            // 
+            this.grImage.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.grImage.FillWeight = 152.2842F;
+            this.grImage.HeaderText = "Image";
+            this.grImage.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.grImage.Name = "grImage";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(177, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.Location = new System.Drawing.Point(830, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnSaveInvoice_Click);
+            // 
+            // tb_ProductID
+            // 
+            this.tb_ProductID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_ProductID.Location = new System.Drawing.Point(177, 79);
+            this.tb_ProductID.Name = "tb_ProductID";
+            this.tb_ProductID.Size = new System.Drawing.Size(154, 26);
+            this.tb_ProductID.TabIndex = 0;
+            // 
+            // tb_Quantity
+            // 
+            this.tb_Quantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Quantity.Location = new System.Drawing.Point(177, 111);
+            this.tb_Quantity.Name = "tb_Quantity";
+            this.tb_Quantity.Size = new System.Drawing.Size(154, 26);
+            this.tb_Quantity.TabIndex = 0;
+            // 
+            // tb_UnitPrice
+            // 
+            this.tb_UnitPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_UnitPrice.Location = new System.Drawing.Point(177, 143);
+            this.tb_UnitPrice.Name = "tb_UnitPrice";
+            this.tb_UnitPrice.Size = new System.Drawing.Size(154, 26);
+            this.tb_UnitPrice.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(62, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "ProductID";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(62, 111);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Quantity";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(62, 143);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "UnitPrice";
+            // 
+            // tb_ItemID
+            // 
+            this.tb_ItemID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_ItemID.Location = new System.Drawing.Point(177, 47);
+            this.tb_ItemID.Name = "tb_ItemID";
+            this.tb_ItemID.Size = new System.Drawing.Size(154, 26);
+            this.tb_ItemID.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(62, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 20);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "ItemID";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(359, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tb_add_pic
+            // 
+            this.tb_add_pic.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tb_add_pic.Location = new System.Drawing.Point(359, 207);
+            this.tb_add_pic.Name = "tb_add_pic";
+            this.tb_add_pic.Size = new System.Drawing.Size(191, 42);
+            this.tb_add_pic.TabIndex = 3;
+            this.tb_add_pic.Text = "Add Picture";
+            this.tb_add_pic.UseVisualStyleBackColor = true;
+            this.tb_add_pic.Click += new System.EventHandler(this.bt_add_pic_Click);
+            // 
+            // tb_InvoiceDate
+            // 
+            this.tb_InvoiceDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tb_InvoiceDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.tb_InvoiceDate.Location = new System.Drawing.Point(830, 85);
+            this.tb_InvoiceDate.Name = "tb_InvoiceDate";
+            this.tb_InvoiceDate.Size = new System.Drawing.Size(154, 20);
+            this.tb_InvoiceDate.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 672);
+            this.Controls.Add(this.tb_InvoiceDate);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tb_add_pic);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvItems);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_TotalAmount);
+            this.Controls.Add(this.tb_UnitPrice);
+            this.Controls.Add(this.tb_Quantity);
+            this.Controls.Add(this.tb_SupplierID);
+            this.Controls.Add(this.tb_ItemID);
+            this.Controls.Add(this.tb_ProductID);
+            this.Controls.Add(this.tb_InvoiceID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItems)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_InvoiceID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_SupplierID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_TotalAmount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvItems;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tb_ProductID;
+        private System.Windows.Forms.TextBox tb_Quantity;
+        private System.Windows.Forms.TextBox tb_UnitPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb_ItemID;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button tb_add_pic;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ItemID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn InvoiceID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UnitPrice;
+        private System.Windows.Forms.DataGridViewImageColumn grImage;
+        private System.Windows.Forms.DateTimePicker tb_InvoiceDate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace invoice_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_add_pic_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Choose Image(*.jpg;*.jpeg;*.pnp)|*.jpg;*.jpeg;*.pnp";
+            if(ofd.ShowDialog() == DialogResult.OK )
+            {
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            dgvItems.Rows.Add(tb_ItemID.Text, tb_InvoiceID.Text, tb_ProductID.Text, tb_Quantity.Text, tb_UnitPrice.Text,pictureBox1.Image);
+            tb_ItemID.Clear();
+            tb_ProductID.Clear(); 
+            tb_Quantity.Clear();
+            tb_UnitPrice.Clear();
+            pictureBox1.Image=null;
+        }
+
+        private byte[] ImageToByteArray(Image image)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+
+        private void btnSaveInvoice_Click(object sender, EventArgs e)
+        {
+
+
+            string connectionString = "Data Source=(LocalDb)\\Local;Initial Catalog=\"update database test\";Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Insert invoice
+                string insertInvoiceQuery = "INSERT INTO Invoices (InvoiceID, InvoiceDate, SupplierID, TotalAmount) VALUES (@InvoiceID, @InvoiceDate, @SupplierID, @TotalAmount)";
+                using (SqlCommand command = new SqlCommand(insertInvoiceQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@InvoiceID", tb_InvoiceID.Text);
+                    command.Parameters.AddWithValue("@InvoiceDate", tb_InvoiceDate.Text);
+                    command.Parameters.AddWithValue("@SupplierID", tb_SupplierID.Text);
+                    command.Parameters.AddWithValue("@TotalAmount", tb_TotalAmount.Text);
+                    command.ExecuteNonQuery();
+                }
+
+                // Insert invoice items
+                string insertItemsQuery = "INSERT INTO InvoiceItems (ItemID, InvoiceID, ProductID, Quantity, UnitPrice, Image) VALUES (@ItemID, @InvoiceID, @ProductID, @Quantity, @UnitPrice, @Image)";
+                foreach (DataGridViewRow row in dgvItems.Rows)
+                {
+                    if (row.IsNewRow) continue;
+
+                    using (SqlCommand command = new SqlCommand(insertItemsQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@ItemID",row.Cells["ItemID"].Value.ToString());
+                        command.Parameters.AddWithValue("@InvoiceID", tb_InvoiceID.Text);
+                        command.Parameters.AddWithValue("@ProductID", row.Cells["ProductID"].Value.ToString());
+                        command.Parameters.AddWithValue("@Quantity", row.Cells["Quantity"].Value.ToString());
+                        command.Parameters.AddWithValue("@UnitPrice", row.Cells["UnitPrice"].Value.ToString());
+                        command.Parameters.AddWithValue("@Image", (byte[])row.Cells["grImage"].Value);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            MessageBox.Show("Invoice saved successfully!");
+        }
+    }
+}
